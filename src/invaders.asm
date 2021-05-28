f8324e491df60675621d09c7f8ce643881fa33fb
karng87 <karng87@gmail.com>
2021-05-28 10:47:55 +0900

    03-section boot_signature start=7dfeh

--- src/invaders.asm
+++ src/invaders.asm
@@ -7,9 +7,7 @@ org 0x_7c00
 ;; $ symbol denotes the current address of the statement
 ;; $$ symbol denotes the address of the beginnig of current section
 ;dup 510-($-$$) db 0 
-times 510-($-$$) db 0 
+;times 510-($-$$) db 0 
+section boot_signature start=0x_7dfe  ;; 7dfe - 7c00 = 1fe(510)
 
-; section boot signature start = 0x_7dfe
-;; 7dfe - 7c00 = 1fe(510)
-;; boot signature
-dw 0x_aa55
+dw 0x_aa55                            ;; boot signature
