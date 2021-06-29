d2f6d0e6862abac00c2170033cdaee67a8a3c176
karng87 <karng87@gmail.com>
2021-06-29 19:30:32 +0900

    gcc -S, gcc -c, ld -L..

---
+++ x86_64/ctuto.s
@@ -0,0 +1,30 @@
+	.file	"ctuto.c"
+	.text
+	.globl	mylong
+	.data
+	.align 8
+	.type	mylong, @object
+	.size	mylong, 8
+mylong:
+	.quad	9
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	$4, -8(%rbp)
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 11.1.0"
+	.section	.note.GNU-stack,"",@progbits
