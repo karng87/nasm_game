82cdba48d6eab0c04f5d7f7deea011fd88eef05c
karng87 <karng87@gmail.com>
2021-07-13 17:19:36 +0900

    io_d2s version 0.0.2

--- x86_64/inc/test.asm
+++ x86_64/inc/test.asm
@@ -1,15 +1,12 @@
 ;; rbx, r12 ~ r15
-section .bss
-
-section .data
 section .text
-;;%include "inc/x64_print.inc"
 global main
-  main:
-    mov rdi, 0xffff_ffff_ffff_ffff
-    call p_xd2s
-    call exit
+main:
+    mov rdi, 2
+    mov rsi, "fd.txt"
+    mov rdx, 0xffff_ffff_ffff_ffff
 
+    call io_d2s
 
 exit:
     mov rax, 60
@@ -17,36 +14,7 @@ exit:
     syscall
     ret
 
-p_xd2s:
-    .prologue:
-        push rbp
-        mov rbp, rsp
 
-    .body:
-        mov r10,rdi 
-        call p_x2s
-        call p_newline
-
-        mov rdi, r10
-        call is_negative
-        mov rdi, r10
-          test rax, rax
-            jz .positive
-        call g_2complement
-          push rax
-
-          mov rdi, '-'
-          call p_char
-
-          pop rdi
-
-      .positive:
-        call p_d2s 
-
-    .epilogue:
-        mov rsp, rbp
-        pop rbp
-        ret
 g_trim:
       mov rax, rdi
       xor rcx, rcx
@@ -79,46 +47,124 @@ g_2complement:
     add rax, 1
     ret
 
-p_d2s:
-    push rbp
-    mov rbp, rsp
+io_char:
+    .prologue:
+      push rbp
+      mov rbp, rsp
 
-    mov r8, rdi
-    mov rsi, 10
-    call g_trim
-    sub rsp, rax ;;max 21
-    mov r9, rax
-    mov rdi, rax
-    dec rdi
-
-    mov rax, r8
-    mov rcx, 10
-    .for:
-      xor rdx, rdx
-      div rcx
-          cmp rdx, 0xa
-            jge .over_a
-        add rdx, '0'
-          jmp .next
-     .over_a:
-          add rdx, 55
-    .next:
-        mov [rsp + rdi], dl
-        dec rdi
-          test rax, rax
-            jz .print
-      jmp .for
-
-    .print:
+    .body:
+      push rsi
       mov rax, 1
-      mov rdi, 1
+      ;mov rdi, rdi
       mov rsi, rsp
-      mov rdx, r9
+      mov rdx, 8
       syscall
 
-    mov rsp, rbp
-    pop rbp
-    ret
+    .epilogue:
+      mov rsp, rbp
+      pop rbp
+      ret
+
+g_fd:
+  
+    push rdi  ;; fine name
+    mov rax, 2
+    mov rdi, rsp
+    mov rsi, 0b_010_001_000_010
+    mov rdx, 0b_110_000_000
+    syscall
+    pop rdi
+      cmp rax, 0
+        jg .end
+    .err:
+      mov rdi, 1
+      mov rsi, "E:g_fd"
+      call io_char
+      call exit
+
+  .end:
+   ret 
+
+io_d2s:
+    .prologue:
+        push rbp
+        mov rbp, rsp
+
+    .body:
+        mov r9, rdi   ;; fd
+        mov r10, rsi  ;; file name
+        mov r8, rdx   ;; data
+
+        cmp rdi, 1
+          je .stdout
+        cmp rdi, 2
+          jge .io_file
+              mov rdi, 1
+              mov rsi, "E:io_d2s"
+              call io_char
+              call exit
+      .io_file:
+        test rsi, rsi
+          jnz .filename
+              mov rdi, 1
+              mov rsi, "E:fname"
+              call io_char
+              call exit
+      .filename:
+        mov rdi, rsi
+        call g_fd
+        mov r9, rax
+        
+      .stdout:
+        mov rdi, r8
+        call is_negative
+          cmp rax, 1
+            jne .not_negative
+        mov rdi, r8
+        call g_2complement
+        mov r8, rax
+          mov rdi, r9
+          mov rsi, '-'
+            call io_char
+
+      .not_negative:
+          mov rdi, r8
+          mov rsi, 10
+            call g_trim
+              sub rsp, rax ;;max 21
+              mov r11, rax
+              mov rdi, rax
+              dec rdi
+      .init:
+          mov rax, r8
+          mov rcx, 10
+      .for:
+          xor rdx, rdx
+          div rcx
+            cmp rdx, 0xa
+              jge .over_a
+          add rdx, '0'
+          jmp .next
+      .over_a:
+          add rdx, 87
+      .next:
+          mov [rsp + rdi], dl
+          dec rdi
+            test rax, rax
+              jz .print
+          jmp .for
+
+      .print:
+          mov rax, 1
+          mov rdi, r9
+          mov rsi, rsp
+          mov rdx, r11
+          syscall
+
+    .epilogue:
+        mov rsp, rbp
+        pop rbp
+        ret
 
 p_x2s:
     .prologue:
@@ -129,7 +175,8 @@ p_x2s:
             mov r8, rdi
 
             mov rdi, '0x_'
-            call p_char
+            mov rsi, 1
+            call io_char
 
             mov rdi, r8
             mov rsi, 16
@@ -170,7 +217,7 @@ p_x2s:
         pop rbp
         ret
 
-p_str:
+p_ptr:
     push rbp
     mov rbp, rsp
 
@@ -210,20 +257,6 @@ p_num:
     pop rbp
     ret
 
-p_char:
-    push rbp
-    mov rbp, rsp
-
-    push rdi
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, rsp
-    mov rdx, 8
-    syscall
-
-    mov rsp, rbp
-    pop rbp
-    ret
 
 p_newline:
     push rbp
