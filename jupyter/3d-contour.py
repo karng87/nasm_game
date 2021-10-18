86567e7b6b861bedbceba109f02ed00fa2ef3d76
karng87 <karng87@gmail.com>
2021-10-18 19:43:36 +0900

    jupyter

---
+++ jupyter/3d-contour.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def f(x,y):
+    return np.sin(np.sqrt(x**2 + y**2))
+
+x = np.linspace(-6,6,30)
+y = np.linspace(-6,6,30)
+
+X,Y = np.meshgrid(x,y)
+Z = f(X,Y)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.contour3D(X,Y,Z,50, cmap='binary')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+plt.show()
+
+####### ax.plot_Wireframe : 
+
+##### ax.plot_surface(x,y,z,rstride=1, cstride=1 cmap='viridis'
---
+++ jupyter/matplotlib.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.02)
+
+plt.figure()
+plt.plot(t2, f(t2), 'k')
+plt.show()
