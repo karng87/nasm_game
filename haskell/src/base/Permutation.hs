ba68b26c50c96a179ec68fb20b1965fe4b34087a
karng87 <karng87@gmail.com>
2022-01-14 00:19:37 +0900

    asus

--- haskell/src/base/Permutation.hs
+++ haskell/src/base/Permutation.hs
@@ -6,21 +6,25 @@ import Distribution.Simple.Utils (xargs)
 -- append ++
 -- cons :
 
-subs :: [a] -> [[a]]
-subs  [] = [[]]
-subs (x:xs) =  (subs xs)++ map (x:) (subs xs)
+-- power set = 2^{n} => to be or not to be per element
+pset:: [a] -> [[a]]
+pset [] = [[]]
+pset (x:xs) =  pset xs ++ map (x:) (pset xs)
 
 -- Conbination nCr
 combi :: [a] -> Int -> [[a]]
-combi xs n = filter (\x -> length x == n) (subs xs)
+combi xs n = filter (\x -> length x == n) (pset xs)
 
 -- Permutation nPr
 permations  xs = do
   x <- xs
-  y <- delete x xs 
+  y <- delete x xs -- delete \iff import Data.List
   z <- delete y (delete x xs)
   return [x, y, z]
 
 permu :: Eq a => [a] -> Int -> [[a]]
 permu xs 0 = [[]]
-permu xs n = xs >>= (\x -> (x:) <$> (permu (delete x xs) (n-1)) )
+permu xs n = xs >>= (\x -> (x:) <$> permu (delete x xs) (n-1) )
+
+p xs = permu xs <$> [1..length xs]
+pj xs = join $ p xs --  join \iff import Control.Monad
