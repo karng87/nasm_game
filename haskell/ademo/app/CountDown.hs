2642ad7a3348c710189b6e9b5589e40a896529ca
karng87 <karng87@gmail.com>
2022-01-29 18:39:40 +0900

    iamc

--- haskell/ademo/app/CountDown.hs
+++ haskell/ademo/app/CountDown.hs
@@ -4,6 +4,7 @@ import System.IO
 import System.CPUTime
 import Numeric
 import Data.List -- delete
+import Control.Applicative (Applicative(liftA2))
 
 main :: IO ()
 main =  do 
@@ -23,9 +24,10 @@ op1 :: Op
 op1 = Div
 
 data Expr = Val Int | App Op Expr Expr
-expr1 :: Expr
-expr1 = App Add (App Mul (App Div (Val 6)(Val 3)) (App Sub (Val 8)(Val 2))) (App Sub (Val 9)(Val 3))
-expr2 = App Add (App Mul (App Div (Val 6)(Val 3)) (App Sub (Val 8)(Val 2))) (App Sub (Val 3)(Val 9))
+expr :: Expr
+expr = App Add (App Mul (App Div (Val 3)(App Add (Val 2)(Val 9))) (App Sub (Val 1)(Val 2))) (App Sub (Val 6)(Val 3))
+--expr = App Add (App Mul (App Div (Val 6)(Val 3)) (App Sub (Val 8)(Val 2))) (App Sub (Val 9)(Val 3))
+--expr = App Add (App Mul (App Div (Val 3)(Val 29)) (App Sub (Val 1)(Val 2))) (App Sub (Val 3)(Val (-3)))
 
 instance Show Op where
    show Add = "+"
@@ -37,7 +39,7 @@ instance Show Expr where
   show (Val n)    = show n
   show (App o l r)= wh l ++ show o ++ wh r where -- op :: infix binary operators
                     wh (Val n) = show n  -- show :: a -> [Char]
-                    wh expr = "(" ++ show expr ++ ")"
+                    wh olr  = "(" ++ show olr ++ ")"
 
 -- all Expr > 0 && natural number
 valid :: Op -> Int -> Int -> Bool
@@ -48,9 +50,15 @@ valid Div x y = x `mod` y == 0
 
 values :: Expr -> [Int]
 values (Val n) = [n]
-values App o l r = values l >>= \l' -> values r >>= \r' -> return l' ++ r'
--- values (App _ l r) = values l ++ values r
-
+values (App _ l r) = values l ++ values r
+{- 
+App Mul (Add (Val 2)(div Val 9 Val 3)) (App Sub Val 3 Val 1)
+  m || (a 2 (d 9 3)) || (s 3 1) || = [2 9 3 3 1]
+  f (a 2 (d 9 3))  ++ f (s 3 1)  === [2,9,3] ++ f(s 3 1) == [2,9,3] ++ [3,1] = [2,9,3,3,1]
+  f l :: f (a 2 (d 9 3))  = f 2 ++ f (d 9 3) = [2] ++ f(d 9 3) => [2} ++ [9,3] = [2,9,3]
+    f(d 9 3) => f 9 ++ f 3 = [9] ++ [3] = [9,3]
+  f(s 3 1) ::: f 3 ++ f 1 = [3,1]
+-}
 -- TODO unknown action
 --values (App _ l r) = values l >>= \l' -> values r  >>= \r' -> [0,l',r',9]
 
@@ -63,34 +71,85 @@ apply Div x y = x `div` y
 
 eval :: Expr -> [Int]
 eval (Val n)     = [n | n > 0]
-eval (App op l r) = eval l >>= \l' -> eval r >>= \r' -> [valid op l' r'] >>= \v -> if v then [apply op l' r'] else []
+eval (App op l r) = eval l >>= (\l' -> eval r >>= (\r' -> [valid op l' r'] >>= (\v -> if v then apply op l' r' : [] else [])))
+-- eval (App op l r) = fmap (apply op) (eval l) <*> eval r 
+-- eval (App op l r) = (apply op) <$> (eval l) <*> eval r 
 -- eval (App op l r) = [apply op l' r' | l' <- eval l, r' <- eval r, valid op l' r']
 -- eval (App op l r) = zipWith (apply op) (eval l) (eval r)
+{- 
+App (Add (Add (Val 2)(Val 9)) (App Sub (Val 6)(Val 3))
+e mul (add 2 9) 3 =  (e add 2 9) >>= ..e 3 .. v mul l' 3 >>= a mul l' 3 : [] 
+e mul (add 2 9) 3 =  (e add 2 9) >>= ..e 3 .. v mul l' 3 >>= a mul l' 3 : [] 
+e mul (add 2 9) 3 =  [5] >>= \5 -> ..e 3 .. v mul 5 3 >>= a mul 5 3 : []  == [15]
+:: e add 2 9 = e 2 >>= \2 -> e 3 >>= \3 -> add 2 3 >>= [5]
 
+App Mul (Add (Val 2)(div Val 9Val 3)) (App Sub Val 3 Val 1)
+eval mul (add 2 (div 9 3)) (sub 6 3) = e (add 2 (div 9 3)) >>= \l' -> e (sub 6 3) >>= \r' -> ""appy mul l' r' : []""
+e (add 2 (div 9 3)) >>= \l' -> e (sub 6 3) >>= \r' -> ""appy mul l' r' : []""
+  <= l ' :: e (add 2 (div 9 3))  ==> e 2 >>= \2 -> <<< e (div 9 3) >>>  >>= \l'' -> "" apply add 2 l'' :[] === add 2 3 ==== [5]
+    <= <<div 9 3>> :: e (div 9 3) ==> e 9 -> e 3 -> div 9 3 : [] === [3]
+  <= r' :: e (sub 6 3) ==> e 6 e 3 sub 6 3 = [3]
+-}
 -- Combinatial functions
 subs :: [a] -> [[a]]
 subs []     = [[]]
 subs (x:xs) = subs xs ++ map (x:) (subs xs) -- = yss ++ map (x:) yss where yss = subs xs
 
-bsubs :: [a] -> [[a]]
-bsubs [] = [[]]
-bsubs (x:xs) = bsubs xs >>= \x' -> [x'] ++ [x : x']
-
--- nPr = permutation $ _nP_r = \frac{n!}{(n-r)!}$
-permutation :: Eq a => [a] -> [[a]]
-permutation []  = [[]]
-permutation (x:xs)  =  
-
-
---permutation np r = np >>= (\x -> (x:) <$> permutation (delete x np) (r-1))
-
--- power set 2^{n}
+{-  power sets = 2^n
+[1,2,3] -> [[],[1],[2],[3],[1,2],[1,3],[2,3],[1,2,3]]
+[] -> [[]]
+[3] ++-> [[3]]
+[2,3] ++-> [[2],[2,3]]
+[1,2,3] ++-> [[1],[1,2],[1,3],[1,2,3]]
+(x:xs) 
+f [] = [[]]
+f x = [[x]]
+f (x:xs) =  f xs ++ [x] : f xs
+-}
 powerset :: [a] -> [[a]]
 powerset [] = [[]]
-powerset (n:ns) =  powerset ns  ++  map (n:) (powerset ns)
--- powerset (n:ns) =  powerset ns  ++  ((n:) <$> powerset ns)
+powerset [x] = [[x]]
+powerset (x:xs) =  powerset xs ++ fmap (x:) (powerset xs)
+{- 
+f [1,2,3] = (f [2,3])                             ++[1]:  (f [2,3])
+f [1,2,3] = ((f[3]) ++[2]: (f[3]))                ++[1]:  (f [2,3])
+f [1,2,3] = ((f[]) ++[3]: (f[])) ++[2]: (f[3]))   ++[1]:  (f [2,3])
+f [1,2,3] = ((f[]) ++[3]: (f[])) ++[2]: ((f[]) ++[3]: (f[])))   ++[1]:  (f [2,3])
+f [1,2,3] = ([[]] ++[3]: [[]]) ++[2]: (([[]] ++[3]: [[]]))   ++[1]:  (f [2,3])
+f [1,2,3] = ([[],[3],[2],[3]])   ++[1]:  (f [2,3])
+-- need fmap -}
 
--- nCr = Combination $ _nC_r = \frac{_nP_r}{r!}$
+{-  interleave 
+[1,2,3] -> (1)23:::[1,2,3],[1,3,2], 1(2)3):::[2,1,3],[2,3,1]  12(3):::[3,1,2],[3,2,1]
+f x [] -> [[]]
+f x [3] x:[]:x-> [[x,3],[3,x]]
+f x [2,3] -> [[x,2,3],[2,x,3],[2,3,x]]
+f x [1,2,3] -> [[x,1,2,3],[1,x,2,3],[1,2,x,3],[1,2,3,x]]
+
+f x [1,2,3] =  (x:1:2:3:[]) : fmap (1:) (f x [2,3])
+f x [1,2,3] =  [x,1,2,3] : fmap (1:) ([x,2,3] : fmap (2:) (f x [3]))
+f x [1,2,3] =  [x:1:2,3] : fmap (1:) ([x,2,3] : fmap (2:) ([x:3]: (fmap (3:) f x [])))
+f x [1,2,3] =  [x:1:2,3] : fmap (1:) ([x,2,3] : fmap (2:) ([x,3]: (fmap (3:) [[x]]))
+f x [1,2,3] =  [x:1:2,3] : fmap (1:) ([x,2,3] : fmap (2:) ([x,3]: [[3,x]]))
+f x [1,2,3] =  [x:1:2,3] : fmap (1:) ([[x,2,3],[2,x,3],[2,3,x]]))
+f x [1,2,3] =  [x:1:2,3] : [[1,x,2,3],[1,2,x,3]]))
+f x [1,2,3] =  [[x:1:2,3],[1,x,2,3],[1,2,x,3]]))
+-}
+interl :: a -> [a] -> [[a]]
+interl x [] = [[x]]
+interl x (y:ys) = (x:y:ys) : fmap (y:) (interl x ys)
+{-  conbinatio = nCr
+[1,2,3] -> (1)23:::[1,2,3],[1,3,2], 1(2)3):::[2,1,3],[2,3,1]  12(3):::[3,1,2],[3,2,1]
+[] -> [[]]
+[3] -> [[3]]
+[2,3] interleave -> [[2,3],[3,2]] :: [1,3] -> [[1,3],[3,1]] :: [1,2] -> [[1,2],[2,1]]
+[1,2,3] interlea-> [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+(x:xs) 
+f [] = [[]]
+f [x] = [[x]]
+f (x:xs) =  [x] ++ ileve xs
+-}
 combination :: [a] -> Int -> [[a]]
 combination _ 0 = [[]] 
 combination n r = take r $ powerset n 
