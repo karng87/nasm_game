936d8401f8c59add766990fb205094b4c8fa21ce
karng87 <karng87@gmail.com>
2022-02-02 16:19:00 +0900

    Lense1,2 compose modify

--- haskell/lense/app/Main.hs
+++ haskell/lense/app/Main.hs
@@ -1,21 +1,22 @@
+{-#LANGUAGE RankNTypes #-}
 module Main where
 
 main :: IO ()
 main = do
-  print $ set (Lense tuple (\ij foo -> foo{tuple=ij})) (9,9) (CFoo (1,1) 'a')
-  print $ set (Lense snd (\j (i,_) -> (i,j))) 8 (2,2) 
-  print $ set (composeL (Lense tuple (\ij foo -> foo{tuple=ij})) (Lense snd (\j (i,_) -> (i,j)))) 7 (CFoo (3,3) 'a')
-  print $ view (modify (Lense tuple (\ij foo -> foo{tuple=ij})) (\(i,j) -> (i*i,j*j) )) (CFoo (3,3) 'a')
-  print $ set (modify (Lense tuple (\ij foo -> foo{tuple=ij})) (\(i,j) -> (i*i,j*j) )) (1,3) (CFoo (3,3) 'a')
+  print $ set1 (Lense1 tuple (\ij foo -> foo{tuple=ij})) (9,9) (CFoo (1,1) 'a')
+  print $ set1 (Lense1 snd (\j (i,_) -> (i,j))) 8 (2,2) 
+  print $ set1 (compose1 (Lense1 tuple (\ij foo -> foo{tuple=ij})) (Lense1 snd (\j (i,_) -> (i,j)))) 7 (CFoo (3,3) 'a')
+  print $ view1 (modify1 (Lense1 tuple (\ij foo -> foo{tuple=ij})) (\(i,j) -> (i*i,j*j) )) (CFoo (3,3) 'a')
+  print $ modify (Lense2 tuple (\ij foo -> foo{tuple=ij}) (\f foo -> foo{tuple=f (tuple foo)})) (\(i,j) -> (i*i,j*j)) (CFoo (3,3) 'a')
 
-data Lense s a = Lense {view :: s -> a 
-                       ,set :: a -> s -> s}
+data Lense1 s a = Lense1 {view1 :: s -> a, set1 :: a -> s -> s}
+compose1 :: Lense1 s a -> Lense1 a a'  -> Lense1 s a'
+compose1 (Lense1 v1f s1f) (Lense1 v2f s2f) = Lense1 (\s -> v2f (v1f s)) (\a s -> s1f (s2f a (v1f s)) s)
+modify1 :: Lense1 s a -> (a -> a) -> Lense1 s a
+modify1 (Lense1 vf sf) f = Lense1 (f . vf) (\a s -> sf (f . vf $ s) s)
 
-composeL :: Lense s a -> Lense a a'  -> Lense s a'
-composeL (Lense v1f s1f) (Lense v2f s2f) = Lense (\s -> v2f (v1f s)) (\a s -> s1f (s2f a (v1f s)) s)
-
-modify :: Lense s a -> (a -> a) -> Lense s a
-modify (Lense vf sf) f = Lense (f . vf) (\a s -> sf (f . vf $ s) s)
+data Lense2 s a = Lense2 {view2 :: s -> a,set2 :: a -> s -> s ,modify :: (a -> a) -> s -> s}
+type Lense' s a = forall f. Functor f => (a -> f a) -> s -> s
 
 data Foo = CFoo {tuple::(Int,Int), char::Char}
 instance Show Foo where
