9d1e302582b4c5de8c6e561b351ad45a4ad34f22
karng87 <karng87@gmail.com>
2022-02-10 19:18:49 +0900

    imac

--- haskell/tictactoe/app/Main.hs
+++ haskell/tictactoe/app/Main.hs
@@ -9,22 +9,43 @@ data Game = Game{gameBoard:: Board,
                  gamePlayer::Player,
                  gameState:: State
                  } deriving (Eq,Show)
-data Player = PlayerX | PlayerO deriving (Eq, Show)                 
+data Player = PlayerX | PlayerO deriving (Eq, Show)
 data State = Running | GameOver (Maybe Player) deriving (Eq,Show)
-type Board = Array(Int,Int) Cell
-data Cell = Empty | Full Player deriving (Eq Show)
+type Board = Array (Int,Int) Cell
+data Cell = Empty | Full Player deriving (Eq, Show)
 
-
+initialGame = Game{
+    gameBoard = array ((0,0),(2,2)) $ zip (range ((0,0),(2,2))) (repeat Empty),
+    gamePlayer = PlayerX,
+    gameState = Running
+  }
 window :: Display
 window = InWindow "Tic Tac Toe" (640, 480) (100, 100)
 
 bgcolor:: Color
 bgcolor = makeColor 0 0 0 255
 
-picture :: Float -> Picture
-picture _ = Blank 
+gamePicture :: Game -> Picture
+gamePicture game = case gameState game of
+    Running -> Blank
+    GameOver winner -> color (makeColor 255 50 50 255) (boardAsPicture $ gameBoard game)
 
-eventHandler :: Event -> game -> game
+boardAsPicture:: Board -> Picture 
+boardAsPicture board = pictures [xCellOfBoard board,oCellOfBoard board, boardGrid]
 
+xCellOfBoard :: Board -> Picture 
+xCellOfBoard = Blank
+
+oCellOfBoard :: Board -> Picture 
+oCellOfBoard = Blank
+
+boardGrid :: Board -> Picture 
+boardGrid = Blank
+
+cellsOfBoard :: Board -> Cell -> Picture 
+cellsOfBoard board cell cellPicture = 
+      $ map (picture cellPicture . fst)
+      $ filter (\_,e) -> e == cell
+      $ assocs board
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = play window bgcolor 30 initialGame gamePicture (\_ game -> game) (const id)
