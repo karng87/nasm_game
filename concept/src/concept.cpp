48e8bab6fe7b8d69f07117ceb0826a3207e6f571
karng87 <karng87@gmail.com>
2022-02-15 02:10:41 +0900

    concept requires

--- concept/src/concept.cpp
+++ concept/src/concept.cpp
@@ -20,6 +20,28 @@ concept all_differ_c = head_differ::isAllDiffer<P, Types...>();
 template<typename P, typename ... Types>
 concept some_differ_c = (!all_same_c<P, Types...> && !all_differ_c<P, Types...>);
 
+template<typename P, typename ... Types>
+requires all_same_c<P,Types...>
+auto sum(P p, Types... args){
+  std::cout << "requires all _same_c" << std::endl;
+  return (p + ... + args);
+}
+template<typename P, typename ...Qs>
+requires all_differ_c<P,Qs...>
+auto sum(P p, Qs... qs){
+  std::cout << "requires all_differ_c" << std::endl;
+  return (p + ... + qs);
+}
+template<typename P, typename ... Qs>
+requires some_differ_c<P,Qs...>
+auto sum(P p, Qs... qs){
+  std::cout << "requires some_differ_c" << std::endl;
+  return (p + ... + qs);
+}
+
 int main(){
+  std::cout << sum(1,2,3,4,5) << std::endl;
+  std::cout << sum(1.1d,2,3.0f) << std::endl;
+  std::cout << sum(1.1d,2,3.1,3.0f) << std::endl;
   return 0;
 }
