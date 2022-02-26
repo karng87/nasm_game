25b54b6faeb5b07f6daa09ce9b049a07cd8b8cf0
karng87 <karng87@gmail.com>
2022-02-26 12:50:46 +0900

    setup - 02.create instanc

--- vulkan/src/triangle.c
+++ vulkan/src/triangle.c
@@ -20,54 +20,11 @@ void gameLoop();
 void gameClean();
 void createInstance();
 
-static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
-  printf ("validation layer: %s\n" ,pCallbackData->pMessage);
-  return VK_FALSE;
-}
-VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
-    if ((PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT") != NULL){
-        return ((PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT"))(instance, pCreateInfo, pAllocator, pDebugMessenger);
-    } else {
-        return VK_ERROR_EXTENSION_NOT_PRESENT;
-    }
-}
-
-void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
-    if ((PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT") != NULL) {
-        ((PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT"))(instance, debugMessenger, pAllocator);
-    }
-}
-
-char**  getRequiredExtensions() {
-    uint32_t glfwExtensionCount = 0;
-    const char** glfwExtensions;
-    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
-    if (enableValidationLayers) {
-      strcat(glfwExtensions ,VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-    }
-
-    return glfwExtensions;
-}
 /* main */
 int main() {
   winInit(win);
   gameLoop();
   createInstance();
-  {
-    if(!enableValidationLayers){return 0;}
-    VkDebugUtilsMessengerCreateInfoEXT vdumcie = {};
-    {
-      vdumcie.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-      vdumcie.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-      vdumcie.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-      vdumcie.pfnUserCallback = debugCallback;
-    }
-    if(createDebugUtilsMessengerEXT(instance,&vdumcie,NULL,&debugMessenger)){
-      printf("failed to set up debug messenger!\n");
-      exit(-1);
-    }
-  }
   gameClean();
 } // end of main()
 
@@ -83,16 +40,8 @@ void gameClean(){
   vkDestroyInstance(instance,NULL);
   glfwDestroyWindow(win);
   glfwTerminate();
-  if (enableValidationLayers) {
-    DestroyDebugUtilsMessengerEXT(instance, debugMessenger, NULL);
-  }
-
 }
 void createInstance(){
-  if (enableValidationLayers && !checkValidationLayerSupport()) {
-    printf("validation layers requested, but not available!");
-  }
-
    VkApplicationInfo vai={};
    {
      vai.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -105,12 +54,6 @@ void createInstance(){
    {
      vici.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
      vici.pApplicationInfo = &vai;
-     {
-      extensions = getRequiredExtensions();
-      createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
-      createInfo.ppEnabledExtensionNames = extensions.data();
-
-     }
      {
        uint32_t glfwEXtensionCount = 0;
        const char ** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwEXtensionCount);
