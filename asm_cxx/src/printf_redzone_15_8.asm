dee426270eb0aecb26f715084055204535facd4a
karng87 <karng87@gmail.com>
2022-02-28 19:41:31 +0900

    imac

--- asm_cxx/src/printf_redzone_15_8.asm
+++ asm_cxx/src/printf_redzone_15_8.asm
@@ -3,15 +3,16 @@
 extern printf
 
 section .data
-fmt: db "Hello %s!",10,0
-.arg1: db "Lee",0
+fmt: db "Nasm %s!",10,0
+.arg1: db "printf Lee",0
 section .text
 global main
 main:
+  push rbp
   lea rdi, [rel fmt]
   lea rsi, [rel fmt.arg1]
   ;; 최소 스택 영여 red zone (128 byte) + 16 byte * n
-  sub rsp,(6+8+1)*8       ;; int args 6(rdi,rsi,rdx,rcx,r8,r9) float args 8 retrun addr 1
+  ;sub rsp,(6+8+1)*8       ;;(6+8+1) int args 6(rdi,rsi,rdx,rcx,r8,r9) float args 8 retrun addr 1
   call printf wrt ..plt
 .exit:
   mov rax, 60
