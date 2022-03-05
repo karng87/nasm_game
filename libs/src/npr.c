2d15e045566a665d90b54869053f0b06ea788520
karng87 <karng87@gmail.com>
2022-03-05 19:03:25 +0900

    nPr

--- libs/src/npr.c
+++ libs/src/npr.c
@@ -10,24 +10,22 @@ void printp(void * argv,int argc){printf("(");for(int i=0;i<argc;i++){printf("%c
 void printpp(void ** argv,int n,int r){for(int i=0;i<n;i++){printp(*(argv+i),r);}}
 
 int main(){
-  const int n=5;
+  const int n=3;
   const int r=3;
   char * src = malloc(n*1); char c =('A'-0); for(int i=0;i<n;i++){*(src+i)=c++;}
   char ** des = malloc(inpr(n,r)*sizeof(char*)); for(int i=0;i<inpr(n,3);i++){*(des+i)=malloc(r*1);}
-  nPr(src,des,n,3);
-  printpp(des,inpr(5,3),3);
+  nPr(src,des,n,0);
+  printpp(des,inpr(n,r),3);
   return 0;
 }
 
-void nPr(char * src, char ** des, int n, int r){
-  //if(n==r){return;}
-  //static int cnt=0;
-  printf("5p3=%d\n",inpr(n,r));
-  printf("5p1=%d\n",inpr(5,1));
-  for(int i=0;i<inpr(n,r);i++){
-    for(int j=0;j<r;j++){
-      *(*(des+i)+j) = *(src+j);
-    }
+void nPr(char * src, char ** des, int n, int si){
+  static int sgi = 0;
+  if(si==1){for(int i=0;i<n;i++){*(*(des+sgi)+i) = *(src+i);}sgi++;return;}
+  for(int i=si;i<n;i++){
+    if(i!=si)*(src+si)^=*(src+i)^=*(src+si)^=*(src+i);
+    nPr(src,des,n,si++);
+    if(i!=si)*(src+si)^=*(src+i)^=*(src+si)^=*(src+i);
   }
 }
 
