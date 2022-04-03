12129d1725594fc0dbee9c5bbf29df06c6816378
karng87 <karng87@gmail.com>
2022-04-03 21:07:13 +0900

    kohi ###5 linux platform layer

--- vulkan/src/main.c
+++ vulkan/src/main.c
@@ -1,4 +1,5 @@
-#include<core/log.h>
+#include "win/windows.h"
+#include <core/log.h>
 
 int main(){
   FATAL("Test FATAL:%f",3.14f);
@@ -7,4 +8,12 @@ int main(){
   INFO("Test INFO:%f",3.14f);
   DEBUG("Test DEBUG:%f",3.14f);
   TRACE("Test TRACE:%f",3.14f);
+  platform_state p_state;
+  if(platform_startup(&p_state, "Xcb", 100, 100, 800, 600)){
+    while(1){
+      platform_pump_messages(&p_state);
+    }
+  }
+  platform_shutdown(&p_state);
+  return 0;
 }
