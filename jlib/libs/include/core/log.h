70601e6ad1ec4317d129357f361f05213eaac369
karng87 <karng87@gmail.com>
2022-04-04 20:01:17 +0900

    imac

---
+++ jlib/libs/include/core/log.h
@@ -0,0 +1,55 @@
+#pragma once
+#include<defines.h>
+
+#define LOG_WARN_ENABLE 1
+#define LOG_INFO_ENABLE 1
+#define LOG_DEBUG_ENABLE 1
+#define LOG_TRACE_ENABLE 1
+
+#if RELEASE == 1
+  #define LOG_DEBUG_ENABLE 0
+  #define LOG_TRACE_ENABLE 0
+#endif
+
+typedef enum log_level{
+  LOG_LEVEL_FATAL=0,
+  LOG_LEVEL_ERROR=1,
+  LOG_LEVEL_WARN=2,
+  LOG_LEVEL_INFO=3,
+  LOG_LEVEL_DEBUG=4,
+  LOG_LEVEL_TRACE=5,
+}log_level;
+
+b8 init_log();
+void shutdown_log();
+API void log_output(log_level,const char*,...);
+
+#define FATAL(msg, ...) log_output(LOG_LEVEL_FATAL, msg, ##__VA_ARGS__)
+
+#ifndef ERROR
+  #define ERROR(msg, ...) log_output(LOG_LEVEL_ERROR,msg,##__VA_ARGS__)
+#endif
+
+#if LOG_WARN_ENABLE == 1
+  #define WARN(msg, ...) log_output(LOG_LEVEL_WARN,msg,##__VA_ARGS__)
+#else
+  #define WARN(msg, ...)
+#endif
+
+#if LOG_INFO_ENABLE == 1
+  #define INFO(msg, ...) log_output(LOG_LEVEL_INFO,msg,##__VA_ARGS__)
+#else
+  #define INFO(msg, ...)
+#endif
+
+#if LOG_DEBUG_ENABLE == 1
+  #define DEBUG(msg, ...) log_output(LOG_LEVEL_DEBUG,msg,##__VA_ARGS__)
+#else
+  #define DEBUG(msg, ...)
+#endif
+
+#if LOG_TRACE_ENABLE == 1
+  #define TRACE(msg, ...) log_output(LOG_LEVEL_TRACE,msg,##__VA_ARGS__)
+#else
+  #define TRACE(msg, ...)
+#endif
---
+++ jlib/src/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <core/log.h>
+  
+struct vec{int dim; double* e;};
+struct orthogonal_basis{double* i;double *j;double *k;};
+struct basis{double* x;double *y;double *z;};
+struct nabula{double p_over_px;double p_over_py;double p_over_pz;};
+struct gradient{double pf_over_px; double pf_over_py; double pf_over_pz;};
+struct divergence{double pux_over_px;double puy_over_py;double puz_over_pz;struct vec dot_du;};
+struct curl{double p_over_px;double p_over_py;double p_over_pz;struct vec cross_du;};
+double dot(struct vec, struct vec);
+void create_vec(struct vec*,int);
+int main(){
+  struct vec v;
+    v.dim=3;
+    v.e = malloc(3*sizeof(double));
+  for(int i=0;i<v.dim;i++){
+    *(v.e+i) = (double)(i+1);
+    printf("%lf,",*(v.e+i));
+  }
+  struct vec u;
+  create_vec(&u,3);
+
+  printf("%lf,%lf\n",dot(u,v),dot(v,u));
+  free(v.e);
+  free(u.e);
+}
+
+void create_vec(struct vec * v,int dim){
+  v->dim=dim,v->e=malloc(dim*sizeof(double));
+  srand(time(NULL));
+  for(int i=0;i<v->dim;i++){
+    *(v->e+i) = (double)(rand());
+    printf("%lf,",*(v->e+i));
+  }
+}
+
+double dot(struct vec v,struct vec u){
+  double res=0;
+  for(int i=0; i<v.dim; i++){
+    res += *(v.e+i) * *(u.e+i);
+  }
+  return res;
+}
+double delta_ij(int i, int j){
+  return (i==j)?1:0;
+}
+double epsilon_ijk(int i, int j, int k){
+  if(i==j||i==k||j==k) return 0;
+  if(i==1){
+    return (j==i+1&&k==i+2) ?1 :-1;
+  }else if(i==2){
+    return (j==i+1&&k==i-1) ?1 :-1;
+  }else if(i==3){
+    return (j==i-1&&k==i-2) ?1 :-1;
+  }else{}
+  FATAL("epsilon_ijk indexing error");
+}
