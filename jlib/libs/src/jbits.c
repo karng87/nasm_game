9c4945f1ae427d06ed8c82bc4becdba8e02b9ede
karng87 <karng87@gmail.com>
2022-04-11 08:50:32 +0900

    jbit

--- jlib/libs/src/jbits.c
+++ jlib/libs/src/jbits.c
@@ -1,24 +1,48 @@
 #include <jbits.h>
-
+#include <stdbool.h>
+// 1010(a) ^= 1100(b) := 0110(a)
+// 1100(b) ^= 0110(a) := 1010(b)
+// 0110(a) ^= 1010(b) := 1100(a)
 void jb_swap(int* a, int* b){*a ^= *b ^= *a ^= *b;}
-void jb_swap_c(char* a, char* b){*a ^= *b ^= *a ^= *b;}
-int jb_odd(int x){return (x&1)?1:0;}
-int jb_even(int x){return !jb_odd(x)?1:0;}
-int jb_is_nth(int x, int n){return (x & (1<<n)) ? 1:0;}
+bool jb_odd(int x){return (x&1)?true:false;}
+bool jb_even(int x){return (x&1)?false:true;}
+bool jb_is_nth(int x, int n){return (x & (1<<n)) ? true:false;}
+// set => or(1<<n)
 int jb_set_nth(int x, int n){int y= x|(1<<n);return y;}
+// unset => and not(1<<n)
 int jb_unset_nth(int x, int n){int y = x & ~(1<<n);return y;}
+// toggel => xor (1<<n)
 int jb_toggle_nth(int x, int n){int y = x ^ (1<<n);return y;}
+// off last => and (x-1)
 int jb_turnoff_rightmost(int x){int y = x & (x-1);return y;}
+
+// 0110100  ( x)
+// 1001100  (-x)
+// get none zero last bit
 int jb_isolate_rightmost_1bit(int x){int y = x &= (-(x));return y;}
+
+// 0110100
+// 0110011
+// last none zero bit -> 전파 시킴
 int jb_propagate_rightmost2right(int x){int y = (x |(x-1));return y;}
+
+// 0110111 ( x)
+// 1001000 (not x)
+// 0111000 (x+1)
+// get the last zero bit
 int jb_isolate_rightmost_0bit(int x){int y = ~(x) & (x+1);return y;}
+
+// 0110111 (  x)
+// 0111000 (x+1)
+// set the last zero bit
 int jb_turnon_rightmost_0bit(int x){int y = x | (x+1);return y;}
 
 // algorithm
+
 void _jb_powerset(int n, char* src,char** dest){
   for(int i=0;i<(1<<n);i++){
-    for(int j=0;j<n;j++){
-      if(i&1<<j){
+    for(int j=0;j<i;j++){
+      if(i&1<<(j+1)){
         *(*(dest+i)+j)= *(src+j);
       }
     }
